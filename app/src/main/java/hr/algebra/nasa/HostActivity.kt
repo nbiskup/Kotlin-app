7d9923a67658aeef7846b524ba3a55605024d6f8
nbiskup <nbiskup@racunartsvo.hr>
2023-02-01 01:26:11 +0100

    Menu items and fragments

--- app/src/main/java/hr/algebra/nasa/HostActivity.kt
+++ app/src/main/java/hr/algebra/nasa/HostActivity.kt
@@ -2,14 +2,72 @@ package hr.algebra.nasa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import hr.algebra.nasa.databinding.ActivityHostBinding
 import hr.algebra.nasa.databinding.ActivitySplashScreenBinding
 
 class HostActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var binding: ActivityHostBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        binding = ActivityHostBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initHamburgerMenu()
+        initNavigation()
+    }
+
+    private fun initHamburgerMenu() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+    }
+
+    private fun initNavigation() {
+        val navController = Navigation.findNavController(this,R.id.navController)
+        NavigationUI.setupWithNavController(binding.navView,navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.host_menu,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                toggleDrawer()
+                return true
+            }
+            R.id.menuExit -> {
+                exitApp()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun exitApp() {
+        AlertDialog.Builder(this).apply {
+            setTitle(R.string.exit)
+            setMessage(R.string.confirm_to_exit)
+            setIcon(R.drawable.exit)
+            setCancelable(true)
+            setPositiveButton(R.string.ok) { _, _ -> finish()}
+            setNegativeButton(R.string.cancel, null)
+            show()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawers()
+        } else{
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 }
