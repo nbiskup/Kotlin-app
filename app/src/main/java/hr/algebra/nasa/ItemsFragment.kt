42188d98128e26460e69b4d4c23bd58bd10b1cb7
nbiskup <nbiskup@racunartsvo.hr>
2023-02-07 00:07:38 +0100

    Created ItemsFragment

--- app/src/main/java/hr/algebra/nasa/ItemsFragment.kt
+++ app/src/main/java/hr/algebra/nasa/ItemsFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import hr.algebra.nasa.adapter.ItemAdapter
+import hr.algebra.nasa.databinding.FragmentItemsBinding
+import hr.algebra.nasa.framework.fetchItems
+import hr.algebra.nasa.model.Item
 
 /**
  * A simple [Fragment] subclass.
@@ -12,15 +17,25 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class ItemsFragment : Fragment() {
+    private lateinit var binding: FragmentItemsBinding
+    private lateinit var items: MutableList<Item>
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_items, container, false)
+        items = requireContext().fetchItems()
+        binding = FragmentItemsBinding.inflate(inflater, container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvItems.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = ItemAdapter(requireContext(), items)
+        }
+    }
 
 }
--- app/src/main/java/hr/algebra/nasa/framework/Extensions.kt
+++ app/src/main/java/hr/algebra/nasa/framework/Extensions.kt
@@ -1,5 +1,6 @@
 package hr.algebra.nasa.framework
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -12,6 +13,8 @@ import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.core.content.getSystemService
 import androidx.preference.PreferenceManager
+import hr.algebra.nasa.NASA_PROVIDER_CONTENT_URI
+import hr.algebra.nasa.model.Item
 
 fun View.applyAnimation(animationId: Int)
     = startAnimation(AnimationUtils.loadAnimation(context, animationId))
@@ -53,4 +56,23 @@ fun callDelayed(delay: Long, runnable: Runnable) {
         runnable,
         delay
     )
+}
+
+@SuppressLint("Range")
+fun Context.fetchItems() : MutableList<Item> {
+    val items = mutableListOf<Item>()
+    val cursor = contentResolver.query(NASA_PROVIDER_CONTENT_URI, null, null, null, null)
+
+    while(cursor != null && cursor.moveToNext()){
+        items.add(Item(
+            cursor.getLong(cursor.getColumnIndex(Item::_id.name)),
+            cursor.getString(cursor.getColumnIndex(Item::title.name)),
+            cursor.getString(cursor.getColumnIndex(Item::explanation.name)),
+            cursor.getString(cursor.getColumnIndex(Item::picturePath.name)),
+            cursor.getString(cursor.getColumnIndex(Item::date.name)),
+            cursor.getInt(cursor.getColumnIndex(Item::read.name)) == 1
+        ))
+    }
+
+    return items
 }
